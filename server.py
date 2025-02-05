11237e577d09c75f65969830c1f17048382da094
UOGus <166261586+UOGus@users.noreply.github.com>
2025-02-04 19:56:52 -0800

    chnaged host to 0.0.0.0 and listening on port 8080

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def wakeup():
-    return "Server awake"
+    return 'server awake'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
