40bec6753a9fc4e3c86e863fa8c9b3cbacebc318
UOGus <166261586+UOGus@users.noreply.github.com>
2025-03-02 20:19:44 -0800

    attack detection now seeing synflood

--- server/attack_detection/pcap_test.c
+++ server/attack_detection/pcap_test.c
@@ -4,14 +4,42 @@
 
 //pcap handler
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
-    printf("Got a packet\n");
+    struct ethheader *eth = (struct ethheader *)packet;
+
+    if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
+        struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
+        
+        switch(ip->iph_protocol) {
+            case IPPROTO_TCP:
+                printf("%s\n", inet_ntoa(ip->iph_sourceip));
+                // const u_char *payload = packet + sizeof(struct ethheader) + sizeof(struct ipheader);
+                // int payload_size = header->len - (payload - packet); 
+                // printf("   Protocol: TCP\n");
+
+                // for(int i = 0; i < payload_size; i++){
+                //     printf("%c", payload[i]);
+                // }
+
+                return;
+            case IPPROTO_UDP:
+                printf("   Protocol: UDP\n");
+                return;
+            case IPPROTO_ICMP:
+                printf("%s\n", inet_ntoa(ip->iph_sourceip));
+                return;
+            default:
+                printf("   Protocol: others\n");
+                return;
+        }
+    }
+    
 }
 
 int main(){
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
     struct bpf_program fp;
-    char filter_exp[] = "icmp";
+    char filter_exp[] = "tcp";
     bpf_u_int32 net;
 
     handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
