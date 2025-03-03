56ee4b995e92b9a125238c06f23d9846044dd27b
UOGus <166261586+UOGus@users.noreply.github.com>
2025-03-02 19:54:26 -0800

    removed teacher comments

--- server/attack_detection/pcap_test.c
+++ server/attack_detection/pcap_test.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include "packet_structs.h"
 
-/* This function will be invoked by pcap for each captured packet.
-We can process each packet inside the function.
-*/
+//pcap handler
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
     printf("Got a packet\n");
 }
@@ -15,21 +13,17 @@ int main(){
     struct bpf_program fp;
     char filter_exp[] = "icmp";
     bpf_u_int32 net;
-    // Step 1: Open live pcap session on NIC with name eth3.
-    // Students need to change "eth3" to the name found on their own
-    // machines (using ifconfig). The interface to the 10.9.0.0/24
-    // network has a prefix "br-" (if the container setup is used).
+
     handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
-    // Step 2: Compile filter_exp into BPF psuedo-code
+
     pcap_compile(handle, &fp, filter_exp, 0, net);
     if (pcap_setfilter(handle, &fp) !=0) {
         pcap_perror(handle, "Error:");
         exit(EXIT_FAILURE);
     }
-    // Step 3: Capture packets
+
     pcap_loop(handle, -1, got_packet, NULL);
-    pcap_close(handle); //Close the handle
+    pcap_close(handle);
     return 0;
 }
-// Note: don’t forget to add "-lpcap" to the compilation command.
-// For example: gcc -o sniff sniff.c -lpcap
+
